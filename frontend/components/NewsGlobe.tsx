90c09fa17a2648f4a7da40390fd63816fe867986
biocracy <113276457+biocracy@users.noreply.github.com>
2026-01-17 12:24:21 +0200

    Fix syntax error in DiagRender version

--- frontend/components/NewsGlobe.tsx
+++ frontend/components/NewsGlobe.tsx
@@ -3574,8 +3574,6 @@ export default function NewsGlobe({ onCountrySelect }: NewsGlobeProps) {
                     {/* Version Indicator */}
                     <div className="absolute bottom-2 right-2 z-[100] text-[10px] text-white/30 font-mono hover:text-white/80 cursor-default select-none transition-colors">
                         v0.121.04 DiagRender
-                    </div>        </div>
-            </div >
-
-        );
-    }
+                    </div>
+                </div >
+    );}
