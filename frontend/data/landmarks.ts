ed085c668a2b3dfae580f0d89fb125a7bc59c69c
biocracy <113276457+biocracy@users.noreply.github.com>
2026-01-17 22:35:24 +0200

    Fix: Force add ignored landmarks.ts file

---
+++ frontend/data/landmarks.ts
@@ -0,0 +1,16 @@
+// Map of City Name -> Icon URL
+// For now, we will use a generic "Capital" icon for major cities until we have specific sprites.
+export const CITY_ICONS: Record<string, string> = {
+    "Paris": "/icons/capital_star.png",
+    "London": "/icons/capital_star.png",
+    "Berlin": "/icons/capital_star.png",
+    "New York": "/icons/capital_star.png",
+    "Tokyo": "/icons/capital_star.png",
+    "Kyiv": "/icons/capital_star.png",
+    "Bucharest": "/icons/capital_star.png",
+    "Washington": "/icons/capital_star.png",
+    "Beijing": "/icons/capital_star.png",
+    "Moscow": "/icons/capital_star.png",
+};
+
+export const GENERIC_CITY_ICON = "/icons/city_dot.png";
